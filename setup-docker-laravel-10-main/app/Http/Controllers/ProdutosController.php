31069f3828318d0105b4c9f7f3bd0694ea9d9020
leonardopadilha <leonardopadilha28@gmail.com>
2023-10-05 05:44:27 -0300

    Criando paginação

--- setup-docker-laravel-10-main/app/Http/Controllers/ProdutosController.php
+++ setup-docker-laravel-10-main/app/Http/Controllers/ProdutosController.php
@@ -9,6 +9,6 @@ class ProdutosController extends Controller
 {
     public function index () {
         $findProduto = Produto::all();
-        return view('pages.produtos.paginacao', compact('$findProduto'));
+        return view('pages.produtos.paginacao', compact('findProduto'));
     }
 }
