d799757bba2a18b1f7a348ac36a6cac2ef3e7960
leonardopadilha <leonardopadilha28@gmail.com>
2023-11-04 08:55:24 -0300

    Estruturando função ajax

--- setup-docker-laravel-10-main/public/js/projeto.js
+++ setup-docker-laravel-10-main/public/js/projeto.js
@@ -3,6 +3,25 @@ function deleteRegistroPaginacao(rotaUrl, idDoRegistro) {
     alert(idDoRegistro)
 
     if (confirm('Deseja confirmar a exclusão?')) {
-
+        $.ajax({
+            url: rotaUrl,
+            method: 'DELETE',
+            Headers: {},
+            data: {
+                id: idDoRegistro,
+            },
+            beforeSend: function () {
+                $.blockUI( {
+                    message: 'Carregando...',
+                    timeout: 2000,
+                });
+            },
+        }).done(function (data) {
+            $.unblockUI();
+            console.log(data);
+        }).fail(function (data) {
+            $.unblockUI();
+            alert("Não foi possível buscar os dados");
+        })
     }
 }
